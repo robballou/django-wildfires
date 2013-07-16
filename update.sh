69253165848ae51281d69212e9050d8b4939c3dc
Rob Ballou <rob.ballou@gmail.com>
2013-07-16 08:17:27 -0600

    Changed update script

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ wget http://activefiremaps.fs.fed.us/data/kml/conus_lg_incidents.kmz
 echo '[ ] Unzip data'
 unzip conus_lg_incidents.kmz
 echo '[ ] Update the data in the app'
-python manage.py parseincidents conus_lg_incidents.kml
+python2.7 manage.py parseincidents conus_lg_incidents.kml
 export DATE=`date +"%Y%m%d"`
 echo '[ ] Archiving data as: incident$DATE.kml'
 mv conus_lg_incidents.kml incidents$DATE.kml
